ca3713642d5b787a598c4358543951b977b6b788
luis <effio_1@hotmail.com>
2026-06-09 22:27:14 -0500

    actualizando logos y mas responsividad

--- js/components.js
+++ js/components.js
@@ -1,7 +1,14 @@
 const navbarHTML = `<header class="site-header-premium">
       <nav class="navbar-premium" aria-label="Main Navigation">
-        <a href="index.html" class="brand-logo">
-          <img src="img/logo.svg" alt="Trailing Dreams Logo" class="logo-premium" />
+        <a href="index.html" class="brand-logo logo-premium-dynamic">
+          <svg class="logo-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z"></path>
+            <path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12"></path>
+          </svg>
+          <div class="logo-text">
+            <span class="logo-trailing">Trailing</span>
+            <span class="logo-dreams">Dreams</span>
+          </div>
         </a>
 
         <input type="checkbox" id="menu-toggle" class="menu-checkbox" />
